b6206ab87e34a29890ddf1e8b5c0d4171822abc1
Ali <humaidan@gmail.com>
2022-02-19 10:58:08 +0300

    pointers mac

---
+++ first.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <climits>
+
+using namespace std;
+
+int main()
+{
+    float A[] = { 2.5f, 5.6f, 9, 8, 7};
+    
+    for (auto &x:A) {
+        cout << ++x << endl;
+    }
+
+
+    return 0;
+}
+
